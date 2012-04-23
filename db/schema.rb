bada465b3a4c084c77c6d1fd3cabd242453fb0c4
Meaghan Lewis <moneikmariel@u.northwestern.edu>
2012-04-22 19:12:23 -0500

    add profession to contractors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120421021927) do
+ActiveRecord::Schema.define(:version => 20120423001134) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(:version => 20120421021927) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "profession"
   end
 
 end
