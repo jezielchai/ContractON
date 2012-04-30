2e184bdd7f2c9e976676c9d6f156899b10adacae
Meaghan Lewis <moneikmariel@u.northwestern.edu>
2012-04-30 03:47:07 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,34 @@
 ContractON::Application.routes.draw do
+ 
   root :to => 'static_pages#home'
+ 
+  # RESOURCES
   resources :companies
   resources :contractors
   resources :profiles
   resources :sessions, only: [:new, :create, :destroy]
   resources :contractor_sessions, only: [:new, :create, :destroy] 
+ 
+  # GETS
   get "contractors/relationship"
-
   get "static_pages/home"
-
-  match '/signup', to: 'contractors#signup'
-  match '/csishow', to: 'sessions#show'
-  match '/csignup', to: 'companies#signup'
-  match '/cshow', to: 'companies#show'
-  match '/csignin', to: 'sessions#new'
-  match '/csignout', to: 'sessions#destroy', via: :delete
-  match '/index', to: 'companies#index'
-  match '/help', to: 'static_pages#help'
   get "contractors/signup" 
   get "company/signup"
-  match '/signin', to: 'contractor_sessions#new'
   get "static_pages/home"
   get "static_pages/help"
+
+  # URLS
+  match '/index', to: 'companies#index'
+  match '/help', to: 'static_pages#help'
+  match '/contracotrs_signup', to: 'contractors#signup'
+  match '/companies_show', to: 'sessions#show'
+  match '/companies_signup', to: 'companies#signup'
+  match '/companies', to: 'companies#show'
+  match '/companies_signin', to: 'sessions#new'
+  match '/companies_signout', to: 'sessions#destroy', via: :delete
+  match '/contractors_signin', to: 'contractor_sessions#new'
   match 'companies_search', to: 'companies#search'
   match 'contractors_search', to: 'contractors#search'
-  match '/signup', to: 'company#signup'
-  match '/show', to: 'company#show'
 
 
   # The priority is based upon order of creation:
